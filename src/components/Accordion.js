b1207eda10c4793e20cc71f5994c2c42cfa84aa3
Alison Cordeiro <acordeiro@gmail.com>
2022-05-17 18:27:45 -0700

    Track index with useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map(item => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className='title active'>
+        <div
+          className='title active'
+          // Reminder: NOT onClick={onTitleClick(index)}
+          // onClick={onTitleClick(index)} would call this as soon as our list of items rendered
+          onClick={() => onTitleClick(index)}
+        >
           <i className='dropdown icon'></i>
             {item.title}
         </div>
@@ -15,7 +26,12 @@ const Accordion = ({ items }) => {
     );
   });
 
-  return <div className='ui styled accordion'>{renderedItems}</div>
+  return (
+    <div className='ui styled accordion'>
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 }
 
 export default Accordion;
